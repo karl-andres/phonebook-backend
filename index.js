a9cbdf677615ee320571c6812dc2677c6afff6f3
karl-andres <135479605+karl-andres@users.noreply.github.com>
2025-04-28 17:26:39 -0400

    added ESLint

--- index.js
+++ index.js
@@ -16,74 +16,74 @@ mongoose.connect(url).then(result => {
 })
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
 app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      JSON.stringify(req.body)
-    ].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 app.use(cors())
 
 app.get('/', (request, response) => {
-    response.send('<h1>Phonebook API</h1>')
+  response.send('<h1>Phonebook API</h1>')
 })
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
     .catch(error => next(error))
 })
 
 app.get('/info', (request, response, next) => {
-    Person.countDocuments({}).then(count => {
-        const currentTime = new Date()
-        response.send(
-            `<p>Phonebook has info for ${count} people</p>
+  Person.countDocuments({}).then(count => {
+    const currentTime = new Date()
+    response.send(
+      `<p>Phonebook has info for ${count} people</p>
         <p>${currentTime}</p>`
-        )
-    })
+    )
+  })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
     .catch(error => next(error))
 
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findByIdAndDelete(request.params.id).then(result => {
-        response.status(204).end()
-    })
+  const id = request.params.id
+  Person.findByIdAndDelete(request.params.id).then(result => {
+    response.status(204).end()
+  })
     .catch(error => next(error))
 
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    /**
+  const body = request.body
+  /**
     if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'name or number missing'
@@ -91,53 +91,52 @@ app.post('/api/persons', (request, response, next) => {
     }
     */
 
-    Person.findOne({ name: body.name })
-        .then(existingPerson => {
-            if (existingPerson) {
-                return response.status(400).json({
-                    error: 'name must be unique'
-                })
-            }
-
-            const person = new Person({
-                name: body.name,
-                number: body.number
-            })
-
-            return person.save()
+  Person.findOne({ name: body.name })
+    .then(existingPerson => {
+      if (existingPerson) {
+        return response.status(400).json({
+          error: 'name must be unique'
         })
-        .then(savedPerson => {
-            if (savedPerson) {
-                response.json(savedPerson)
-            }
-        })
-        .catch(error => next(error))
+      }
+
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+
+      return person.save()
+    })
+    .then(savedPerson => {
+      if (savedPerson) {
+        response.json(savedPerson)
+      }
+    })
+    .catch(error => next(error))
 })
 
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
-  
+
 // handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).send({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+
+  next(error)
 }
-  
 // this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`)
 })
